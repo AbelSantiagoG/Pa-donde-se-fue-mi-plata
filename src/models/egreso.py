0a6aad929b805a4708ab217c2867875550db2faf
MiguelAP30 <miguelonperez300@gmail.com>
2024-05-01 19:19:58 -0500

    solucion del CRUD de todas las categorias y el CRUD de ingresos y egresos

--- src/models/egreso.py
+++ src/models/egreso.py
@@ -7,9 +7,9 @@ class Egreso(Base):
 
     id              = Column(Integer, primary_key=True, autoincrement=True)    
     fecha           = Column(Date)    
-    descripcion     = Column(String(length=150))    
-    valor           = Column(Integer)
-    categoria_id    = Column(Integer, ForeignKey("categorias_egresos.id"))
+    description     = Column(String(length=150))    
+    value           = Column(Integer)
+    categoria       = Column(Integer, ForeignKey("categorias_egresos.id"))
 
-    categoria       = relationship("Categoria_Egreso", back_populates="egresos") 
+    categoria_egreso      = relationship("Categoria_Egreso", back_populates="egresos") 
     
--- src/models/ingreso.py
+++ src/models/ingreso.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Float
 from sqlalchemy.orm import relationship 
 from src.config.database import Base
 
@@ -7,8 +7,8 @@ class Ingreso(Base):
 
     id              = Column(Integer, primary_key=True, autoincrement=True)    
     fecha           = Column(Date)    
-    descripcion     = Column(String(length=150))   
-    valor           = Column(Integer)   
-    categoria_id    = Column(Integer, ForeignKey("categorias_ingresos.id"))
+    description     = Column(String(length=150))   
+    value           = Column(Float)   
+    categoria       = Column(Integer, ForeignKey("categorias_ingresos.id"))
 
-    categoria = relationship("Categoria_Ingreso", back_populates="ingresos")
+    categoria_ingreso = relationship("Categoria_Ingreso", back_populates="ingresos")
